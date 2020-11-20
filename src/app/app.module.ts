3e899f9451a29c262fd2df5dd8af2e484d994430
elise young <eliseyoung_@hotmail.com>
2020-11-20 09:39:18 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TodoComponent } from './components/todo.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/directives/forbiddenDate.ts
+++ src/app/directives/forbiddenDate.ts
@@ -12,6 +12,10 @@ export function forbiddenDate(dateVal: string): ValidatorFn {
     return (control: AbstractControl): {[key: string]: boolean} | null => {
         var now = moment().subtract(1, "days")
 
+        /* let now = new Date()    
+        let dueDate = new Date(control.value) 
+        if (dueDate < now) { */
+            
         if (moment(control.value) < now) {
             return { dateIsBefore: true }
         }
